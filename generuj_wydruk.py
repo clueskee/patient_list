c02ad9b860bc370968f680f5ac970711424c9d59
Clueskee <pucekpoczta@gmail.com>
2019-08-19 14:07:55 +0200

    add improvements in code

--- generuj_wydruk.py
+++ generuj_wydruk.py
@@ -3,47 +3,42 @@ from shutil import copyfile
 from weasyprint import HTML
 import os
 
-
-# otwarcie pliku i utowrzenie listy
 def open_list():
-    rlist = open('pacjenci.txt').read().split('\n')
-    return rlist
+    """Opens txt file and make a clean list of patients"""
+    with open('pacjenci.txt') as rlist:
+        return sorted([patient.strip() for patient in rlist if patient.strip()])
 
-# # funkcja zmieniajaca listę
-def clean_list(x):
-    x = sorted(list(filter(None, x)))
-    x = [e.strip() for e in x]
-    return x
+def create_table():
+    """Generates an HTML table with patients"""
+    table_template = '''<tr>
+    <td class="number">{number}</td>
+    td class="patientname">{patient}</td>
+    </tr>
+    '''
+    all_tables=''
 
+    for number, patient in enumerate(open_list(), start=1):
+        all_tables += table_template.format(number=number, patient=patient)
+    return all_tables
 
-patient_list = clean_list(open_list())
-# patient_list = open_list()
-# tworzenie tabeli do umieszczenia w pliku
-def create_table(list):
-    table_cell = '<td class="patientname">'
-    number_cell = '<tr><td class="number">'
-    tmp_table=""
-    for i in range(len(patient_list)):
-        tmp_table += (number_cell + str(i + 1) + '</td>' + table_cell + patient_list[i] + '</td></tr>')
-    return tmp_table
-
-# zapis do pliku
 def write_to_file():
+    """Writes a HTML table to a copy of template HTML file"""
     tablecopy = copyfile('misc/table.html', 'misc/pacjenci.html')
     for line in fileinput.FileInput(tablecopy, inplace=1):
         if "<!-- tabelatutaj -->" in line:
-            line=line.replace(line, line + create_table(patient_list))
+            line=line.replace(line, line + create_table())
         else:
             pass
         print(line, end='')
     return tablecopy
 
-
-def create_pdf():
+def generuj_wydruk():
+    """Removes old .pdf file and create newone from teamplate file by weasyprint library""" 
     try:
         os.remove('pacjenci.pdf')
-    except:
+    except OSError as e:
         pass
     HTML(write_to_file()).write_pdf('pacjenci.pdf')
 
-create_pdf()
+if __name__ == "__main__":
+    generuj_wydruk()
