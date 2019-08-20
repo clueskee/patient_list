311b7d95743dd864bd39adfbc70cde3e6d7ffe79
Clueskee <pucekpoczta@gmail.com>
2019-08-20 19:56:01 +0200

    last commit before add to remote repo

--- generuj_wydruk.py
+++ generuj_wydruk.py
@@ -3,16 +3,27 @@ from shutil import copyfile
 from weasyprint import HTML
 import os
 import logging
+
+# Jest jakiś prosty sposób na log po return()?
+# Jak sobie poradzić z kolejnością logów?
+
 def open_list():
     """Opens txt file and make a clean list of patients"""
-    with open('pacjenci.txt') as rlist:
-        return sorted([patient.strip() for patient in rlist if patient.strip()])
+    logging.info('Otwieram listę pacjentów.')
+    try:
+        with open('pacjenci.txt') as rlist:
+            return sorted([patient.strip() for patient in rlist if patient.strip()])
+    except OSError as e:
+        logging.warning('Błąd pliku pacjenci.txt:', exc_info=True)
+
+
 
 def create_table():
     """Generates an HTML table with patients"""
+    logging.info('Generuję tabelę z listy pacjentów.')
     table_template = '''<tr>
     <td class="number">{number}</td>
-    td class="patientname">{patient}</td>
+    <td class="patientname">{patient}</td>
     </tr>
     '''
     all_tables=''
@@ -23,6 +34,7 @@ def create_table():
 
 def write_to_file():
     """Writes a HTML table to a copy of template HTML file"""
+    logging.info('Zapisuję tabelę z listą pacjentów do pliku.')
     tablecopy = copyfile('misc/table.html', 'misc/pacjenci.html')
     for line in fileinput.FileInput(tablecopy, inplace=1):
         if "<!-- tabelatutaj -->" in line:
@@ -47,4 +59,5 @@ if __name__ == "__main__":
         pass
     finally:
         logging.basicConfig(filename='logs/log.log',level=logging.DEBUG, filemode='w', format='%(asctime)s - %(levelname)s():%(lineno)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S')
+        logging.info('Start programu.')
     generuj_wydruk()
